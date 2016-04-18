c3ccc31b5bacad4f3b59b84564a39d2951e15046
Annie <annie-d@hotmail.ca>
2016-04-18 18:42:12 +0000

    added commented challenge text

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,9 @@
+// First program
+
+// Create a file called hello-world.js . In it, write a simple node program that outputs "Hello World!" 
+// to the console.
+// Add an instruction to your program that will output "Hello World Again!!" 10 seconds after the program was run.
+
 console.log("Hello World!");
 
 setTimeout(function() {
