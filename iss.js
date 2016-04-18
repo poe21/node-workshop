3b89f7c98699b3c617052eb11a6d71308693c163
Annie <annie-d@hotmail.ca>
2016-04-18 18:41:22 +0000

    added commented challenge text

--- iss.js
+++ iss.js
@@ -1,3 +1,13 @@
+// Getting some data
+// Create a file called iss.js. In it, write a simple node program that will output the latitude and longitude 
+// of the International Space Station.
+
+// Practice your google-fu by searching for “iss api” and figuring out the correct URL to use. 
+// Hint: there are many options and they are all good :)
+
+// Notice that the values provided by the API are very precise. Round off the values to two decimal digits 
+// for a nicer display. Hint: toFixed
+
 var request = require("request");
 var address = "https://api.wheretheiss.at/v1/satellites/25544";
 
